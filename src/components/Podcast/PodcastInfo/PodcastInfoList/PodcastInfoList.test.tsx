87d4afffce1655517e8444b04f0a7b6e81c4ffd4
Marc Vinues <mrdk86@gmail.com>
2025-11-17 22:31:41 +0100

    remove param not used

--- src/components/Podcast/PodcastInfo/PodcastInfoList/PodcastInfoList.test.tsx
+++ src/components/Podcast/PodcastInfo/PodcastInfoList/PodcastInfoList.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, within } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { EpisodeList } from './PodcastInfoList';
 import type { Episode } from '../../../../@types/interfaces/podcast';
